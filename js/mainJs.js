47cb846871b61d8ec2c110def27f50873a15742f
anthonydlsantos <agregdelossantos@gmail.com>
2022-10-28 12:38:20 -0300

    Web personal 1.0 - 28/10

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,1 +1,30 @@
 console.log("Hola, que tal? ;) ")
+
+/* --- buttom - homeMenu --- */
+
+btn  = document.getElementsByTagName("button")[0];
+menu = document.getElementById("homeMenu");
+
+cerrar  = document.getElementById("cerrarMenu");
+
+function showMenu() {
+    if ( btn.getAttribute("estado") != "abierto" ){
+        // Si no esta "abierto", agrego attr abierto. 
+        btn.setAttribute("estado","abierto");
+        menu.style.display = "block"; // Abre menu.
+
+        cerrar.onclick = function () {
+            btn.setAttribute("estado","cerrado")
+            menu.style.display = "none"
+        }
+
+    } else {
+        // Si esta "abierto", cambio attr cerrado
+        btn.setAttribute("estado","cerrado")
+        menu.style.display = "none"
+    }
+    
+}
+
+btn.addEventListener( "click", showMenu );
+
